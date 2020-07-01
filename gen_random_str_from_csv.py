3aff46df3247fe573c9fe062f3ee3fcf91ed7eba
atktng <ktng.attc.19@gmail.com>
2020-07-01 20:50:34 +0900

    update

--- gen_random_str_from_csv.py
+++ gen_random_str_from_csv.py
@@ -2,11 +2,16 @@
 
 import csv
 from random import choice
+import sys
 
 
-with open('words.csv') as f:
+if len(sys.argv) != 2:
+    print('Usage: ./gen_rand_str_from_csv.py <csvpath>')
+    sys.exit()
+
+with open(sys.argv[1]) as f:
     rd = csv.reader(f)
     m = [row for row in rd]
 
 for i in range(1000):
-    print(''.join([choice(r) for r in m]))
+    print(' '.join([choice(r) for r in m]))
