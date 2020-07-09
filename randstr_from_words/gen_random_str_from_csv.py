ea00274bdbd07287ba8c318135840e288334b01c
atktng <ktng.attc.19@gmail.com>
2020-07-09 18:47:58 +0900

    add substr func

--- randstr_from_words/gen_random_str_from_csv.py
+++ randstr_from_words/gen_random_str_from_csv.py
@@ -5,6 +5,11 @@ from random import choice
 import sys
 
 
+def get_substr_word(word: str, len: int) -> str:
+    return choice(word.split(" "))[:len]
+
+def get_prefix_word
+
 if len(sys.argv) != 2:
     print('Usage: ./gen_rand_str_from_csv.py <csvpath>')
     sys.exit()
@@ -13,5 +18,8 @@ with open(sys.argv[1]) as f:
     rd = csv.reader(f)
     m = [row for row in rd]
 
-for i in range(1000):
-    print(' '.join([choice(r) for r in m]))
+for i in range(10):
+    word = ' '.join([choice(r) for r in m])
+    print(word)
+
+    print(get_rand_substr(word, 3))
